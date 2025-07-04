20f5034cf9af6f5480a422cda0af6e1f48446e54
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-07-04 16:13:00 +0530

    Initial commit

---
+++ src/Interface/Calculator.java
@@ -0,0 +1,6 @@
+package Interface;
+
+public interface Calculator {
+	
+	public int sum(int a,int b);
+}
---
+++ src/Test/Main.java
@@ -0,0 +1,16 @@
+package Test;
+
+import Interface.Calculator;
+import InterfaceImpl.CalculatorImpl;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Calculator c = new CalculatorImpl();
+		int sum = c.sum(11, 12);
+		System.out.println(sum);
+		
+	}
+
+}
